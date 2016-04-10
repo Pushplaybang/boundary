6932f99858fffbfebf3a9cd38f226b3ca76b5f7d
Paul van Zyl <paul@nonacreative.com>
2016-04-10 11:00:59 +0200

    packaging

---
+++ client/helpers.js
@@ -0,0 +1,50 @@
+Template.Boundary.onCreated(function() {
+  var context = this.data.context;
+  Boundary.subsCount.set(context, 0);
+  Boundary.pageCount.set(context, Boundary.pageIncrement[context]);
+  Boundary.viewCount.set(context, -1);
+});
+
+Template.Boundary.helpers({
+  more: function() {
+    return Boundary.subsCount.get(this.context) > Boundary.pageCount.get(this.context);
+  },
+  ready: function() {
+    return Boundary.ready.get(this.context);
+  }
+});
+
+Template.Boundary.events({
+  'click button': function(event, template) {
+    Boundary.loadMore({context: this.context});
+  }
+});
+
+Template.Boundary.onDestroyed(function() {
+  /* CleanUp and Reset When this template is destroyed */
+    Boundary.reset({context: this.data.context});
+});
+
+Template.BoundarySpinner.onRendered(function() {
+  this.find('.loaderWrap')._uihooks = {
+    insertElement: function (node, next) {
+      $(node)
+        .hide()
+        .insertBefore(next)
+        .fadeIn(200);
+    },
+    removeElement: function(node) {
+      $(node).delay(800).animate({
+        opacity: 0
+      }, 200, function() {
+        $(this).remove();
+      });
+    }
+  };
+});
+
+Template.BoundarySpinner.helpers({
+  ready: function() {
+    return Boundary.ready.get(this.context);
+  }
+});
