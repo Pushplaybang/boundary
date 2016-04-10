fb34a5930e160efc3f808f36e6925178411d9786
Paul van Zyl <paul@nonacreative.com>
2016-04-10 11:01:18 +0200

    package js

---
+++ package.js
@@ -0,0 +1,24 @@
+/* eslint-disable */
+Package.describe({
+  name: 'pushplaybang:boundary',
+  version: '0.0.1',
+  summary: 'flexible isomorphic pagination package for meteor',
+  git: '',
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.2.1');
+  api.use('blaze-html-templates');
+  api.use('standard-minifier-css');
+  api.use('standard-minifier-js');
+  api.use('es5-shim');
+  api.use('ecmascript');
+  api.use('reactive-dict');
+  api.addFiles('lib/server.js', 'server');
+  api.addFiles('lib/Boundary.js', 'client');
+  api.addFiles('client/Boundary.css', 'client');
+  api.addFiles('client/Boundary.html', 'client');
+  api.addFiles('client/helpers.js', 'client');
+  api.export('Boundary', ['client', 'server']);
+});
